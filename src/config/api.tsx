668581ceb437563660ffa1731437b11f0b3d01e8
SylvainMarcosGit <sylvainmarcoszossou@gmail.com>
2025-06-26 13:46:54 +0100

    ajout du fichier env

--- src/config/api.tsx
+++ src/config/api.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api';
+const API_URL = import.meta.env.VITE_API_URL;
+
 
 // Créer une instance axios
 const axiosInstance = axios.create({
